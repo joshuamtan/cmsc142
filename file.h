2d11a90319eb167e4174902b6196788a284b2357
Rain Tomista <rftomista@gmail.com>
2017-11-14 17:47:12 +0800

    Implemented file reading

---
+++ file.h
@@ -0,0 +1,46 @@
+void printCube(int cube[9][12]){
+    int i, j;
+    for(i=0; i<9; i++){
+        for(j=0; j<12; j++){
+            printf("%2d ", cube[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int getColor(char color){
+    if(color == 'W') return 1;
+    else if(color == 'R') return 2;
+    else if(color == 'B') return 3;
+    else if(color == 'O') return 4;
+    else if(color == 'G') return 5;
+    else if(color == 'Y') return 6;
+}
+
+void setFace(FILE * fp, int startX, int startY, int cube[9][12]){
+    int i, j;
+    char a, b, c;
+
+    for(i=startX; i<startX+3; i++){
+        fscanf(fp, "%c%c%c\n", &a, &b, &c);
+        cube[i][startY] = getColor(a);
+        cube[i][startY+1] = getColor(b);
+        cube[i][startY+2] = getColor(c);  
+    }
+}
+
+
+void loadCube(int cube[9][12]){
+    int startX, startY, i, j;
+    FILE * fp = fopen("cube.in", "r");
+
+    setFace(fp, 0, 3, cube); //White
+    setFace(fp, 3, 0, cube); //Red
+    setFace(fp, 3, 3, cube); //Blue
+    setFace(fp, 3, 6, cube); //Orange
+    setFace(fp, 3, 9, cube); //Green
+    setFace(fp, 6, 3, cube); //Yellow
+    fclose(fp);
+
+}
+
--- rubick.c
+++ rubick.c
@@ -1,51 +1,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "file.h"
 
 int indexCol[6] = {4,4,7,10,4,1};
 int indexRow[6] = {1,4,4,4,7,4};
 
-int cube[9][12] = {{0,0,0,1,1,1,0,0,0,0,0,0},
-                   {0,0,0,1,1,1,0,0,0,0,0,0},
-                   {0,0,0,1,1,1,0,0,0,0,0,0},
-                   {6,6,6,2,2,2,3,3,3,4,4,4},
-                   {6,6,6,2,2,2,3,3,3,4,4,4},
-                   {6,6,6,2,2,2,3,3,3,4,4,4},
-                   {0,0,0,5,5,5,0,0,0,0,0,0},
-                   {0,0,0,5,5,5,0,0,0,0,0,0},
-                   {0,0,0,5,5,5,0,0,0,0,0,0}};
+int cube[9][12] = {};
 
 int adjMatrix[6][6] = {{0,1,1,1,0,1},
                        {1,0,1,1,1,1},
                        {1,1,0,0,1,0},
                        {1,0,1,0,1,1},
                        {0,1,1,1,0,1},
-                       {1,1,0,1,1,0}}
+                       {1,1,0,1,1,0}};
 int main(){
-    for(int i=0; i<9; i++){
-        for(int j=0; j<12; j++){
-            printf("%d ",cube[i][j]);
-        }
-        printf("\n");
-    }
-    
+    loadCube(cube);
+    printCube(cube);    
     for(int i=0; i<6; i++){
         printf("%d", cube[indexRow[i]][indexCol[i]]);
     }
 
 }
 
-void rotate_cube(colour, direction){
-    if(direction == "CLOCKWISE"){
-        switch(colour){
-            case 1:
+// void rotate_cube(colour, direction){
+//     if(direction == "CLOCKWISE"){
+//         switch(colour){
+//             case 1:
                 
-                break;
-            default:
+//                 break;
+//             default:
 
-        }
-    }else{
+//         }
+//     }else{
 
-    }
-}
+//     }
+// }
 
